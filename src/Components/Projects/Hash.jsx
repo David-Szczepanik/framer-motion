921f71403a33e726bdeac4c1faa0100aaa292e2c
David-Szczepanik <Davidszczepanik@seznam.cz>
2024-06-20 01:25:36 +0200

    v02 link updated

--- src/Components/Projects/Hash.jsx
+++ src/Components/Projects/Hash.jsx
@@ -2,9 +2,9 @@ function Hash() {
 
   return (
     <>
-      <h1>Hashing file Demo |<a href="http://localhost:8080" className="hover:bg-gray-200 rounded-lg"> <strong>Open in Fullscreen</strong></a>
+      <h1>Hashing file Demo |<a href="https://szczepanik.cz:4000/app.html" className="hover:bg-gray-200 rounded-lg"> <strong>Open in Fullscreen</strong></a>
       </h1>
-      <iframe title="Leaflet App" src="http://localhost:8080" width="100%" height="800px"></iframe>
+      <iframe title="Leaflet App" src="https://szczepanik.cz:4000/app.html" width="100%" height="800px"></iframe>
     </>
   )
 }
--- src/Components/Projects/Leaflet.jsx
+++ src/Components/Projects/Leaflet.jsx
@@ -2,9 +2,9 @@ function Leaflet() {
 
   return (
     <>
-    <h1>Leaflet App |<a href="http://localhost:4200" className="hover:bg-gray-200 rounded-lg"> <strong>Open in Fullscreen</strong></a>
+    <h1>Leaflet App |<a href="https://szczepanik.cz:4200/" className="hover:bg-gray-200 rounded-lg"> <strong>Open in Fullscreen</strong></a>
     </h1>
-    <iframe title="Leaflet App" src="http://localhost:4200" width="100%" height="800px"></iframe>
+    <iframe title="Leaflet App" src="https://szczepanik.cz:4200/" width="100%" height="800px"></iframe>
   </>
   )
 }
